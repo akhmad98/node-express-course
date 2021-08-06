1ab101ed3506fd3320a45d0b10bc32e535289af8
akhmad98 <akhmadbadalov98@gmail.com>
2021-08-06 12:37:38 +0500

    feat: add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+// josn file holding user names
+const mockUserData = [
+    { name: 'Ahmad' },
+    { name: 'Otabek' }
+]
+app.get('/users', function (req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
 
 app.listen(8000, function () {
     console.log("server is running")
