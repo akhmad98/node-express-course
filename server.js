c4cab36ca4a36e6020b3fc077ffd7b78732afe98
akhmad98 <akhmadbadalov98@gmail.com>
2021-08-06 12:53:22 +0500

    feat: add POST route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 // josn file holding user names
 const mockUserData = [
     { name: 'Ahmad' },
     { name: 'Otabek' }
 ]
+
 app.get('/users', function (req, res) {
     res.json({
         success: true,
@@ -19,6 +24,26 @@ app.get('/users/:id', function (req, res) {
         message: "get one's user",
         user: req.params.id
     })
+})
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyMyBoy";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username donot match'
+        })
+    }
 })
 app.listen(8000, function () {
     console.log("server is running")
